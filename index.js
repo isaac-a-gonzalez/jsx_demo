abdfe7469723135c26dd77de0bfa620ff6830142
Isaac A. Gonzalez <isaaca.gonzalez228@gmail.com>
2021-08-06 11:37:57 -0700

    added mood function to diplay a random mood to the dom

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+function getMood() {
+  const moods = ["Angry", "Hungry", "Silly", "Quiet", "Paranoid"];
+  // this is a standard way of grabbing something from an array at random
+  return moods[Math.floor(Math.random() * moods.length)];
+}
+
 class JSXDemo extends React.Component {
   render() {
     return (
       <div>
-        <h1>My number is: {2 * 8.4}</h1>
+        <h1>My Current Mood is: {getMood()}</h1>
       </div>
     );
   }
